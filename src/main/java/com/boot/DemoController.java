a175d4e0e435c96714456fc7816b711e50c6eb3f
yeseong1108 <ingyoeba1108@naver.com>
2025-10-28 15:10:25 +0900

    3

--- src/main/java/com/boot/DemoController.java
+++ src/main/java/com/boot/DemoController.java
@@ -22,7 +22,7 @@ public class DemoController {
 	@RequestMapping("/hello.do")
 //	public String hello() {
 	public String hello(Model model) {
-		log.info("안녕하세요!111 222");
+		log.info("안녕하세요!111 222 333");
 		
 		model.addAttribute("message", "hello.jsp 입니다!");
 		
